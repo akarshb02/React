aea15acd2736eb64ee33d32a4f9f6645b23c2e57
Akarsh <akarshb02@gmail.com>
2020-12-08 20:02:08 +0530

    rendering htim code inside js

---
+++ unordered.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render( <
+    div >
+    <
+    h1 > Fav Foods < /h1> <
+    ul >
+    <
+    li > kebab < /li> <
+    li > chicken 65 < /li> <
+    li > Grill < /li> < /
+    ul > <
+    /div>,
+    document.getElementById("root")
+);
