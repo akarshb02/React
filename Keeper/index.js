4f1c333472b3702526a8c55fd7a92fee05da6ed9
Akarsh <akarshb02@gmail.com>
2020-12-09 20:02:37 +0530

    splited note.jsx file

---
+++ Keeper/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import Header from "./Header";
+import Note from "./Note";
+import Footer from "./Footer";
+
+
+
+ReactDOM.render(
+
+    <
+    div > < h1 >
+    <
+    Header / > < /h1> <
+    Footer / >
+    <
+    Note / >
+    <
+    /div>,
+    document.getElementById("root")
+);
