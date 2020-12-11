bfb7fa7c32915edd751b9b36a83d1ef91b034006
Akarsh <akarshb02@gmail.com>
2020-12-11 13:26:50 +0530

    squaring a number using map function

---
+++ squaring.js
@@ -0,0 +1,12 @@
+var number = [3, 56, 2, 48, 5];
+
+const mul = number.map(function(x) {
+    return x * 2;
+});
+console.log(mul);
+
+const fil = number.filter(function(x) {
+    return x * 2;
+});
+
+console.log(fil);
