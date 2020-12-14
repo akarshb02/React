6bba83319e023930b490731c7dc7a730347e8818
Akarsh <akarshb02@gmail.com>
2020-12-14 20:02:37 +0530

    counter app using distructuring array

---
+++ CounterApp.jsx
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setcount] = useState(0);
+
+  function increase() {
+    setcount(count + 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
